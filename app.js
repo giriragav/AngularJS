a59158a84b07dc62b518814883edd7cdb7bd09c7
venkat-giri <giri.ragav@gmail.com>
2017-05-27 18:01:44 -0500

    Added AngularJS and Dependecy Injection example

--- app.js
+++ app.js
@@ -1,5 +1,22 @@
-myNameSpace.Person = 'Giri'
-myNameSpace.logPerson();
+var myApp = angular.module('myApp',[]);
+
+myApp.controller('mainController',function(){
+    
+});
 
 
+///---------------Dependency Injection-----------------////////
+var Person = function(firstname, lastname){
+    this.firstname = firstname;
+    this.lastname = lastname;
+}
 
+var giri = new Person('Giridharan','Venkatesan');
+
+logPerson(giri);
+function logPerson(person){
+    
+    console.log(person);
+}
+
+///---------------Dependency Injection-----------------////////
